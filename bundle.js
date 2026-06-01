0e21b4824c1a53006646deb2930bc19b889f9ea4
phoenix4ps <basheermohammed3210@gmail.com>
2026-06-01 10:12:31 +0300

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,43 +1,53 @@
-function setStatus(text){
+/* Copyright (C) 2026 anonymous
+This file is part of PHOENIX Framework. */
 
-    document.querySelector(".footer").innerHTML = text;
-
+function setStatus(text) {
+    var footerElement = document.querySelector(".subtitle");
+    if (!footerElement) {
+        footerElement = document.querySelector("div");
+    }
+    
+    if (footerElement) {
+        footerElement.innerHTML = text;
+    }
+    
+    if (typeof window.log === "function") {
+        window.log(text, "#00ffcc");
+    }
 }
 
-function runExploit(){
-
+function runExploit() {
     setStatus("Loading Exploit...");
 
-    setTimeout(() => {
-
+    setTimeout(function() {
         setStatus("Running WebKit...");
-
     }, 2000);
 
-    setTimeout(() => {
-
+    setTimeout(function() {
         setStatus("Escalating Kernel...");
-
     }, 4000);
 
-    setTimeout(() => {
-
+    setTimeout(function() {
         setStatus("Loading GoldHEN...");
-
+        if (typeof window.loadAutoPayload === "function") {
+            window.loadAutoPayload();
+        }
     }, 6000);
 
-    setTimeout(() => {
-
+    setTimeout(function() {
         setStatus("GoldHEN Loaded Successfully");
-
     }, 9000);
-
 }
 
-/* auto start */
-
-window.onload = function(){
-
+window.doJBwithPSFreeLapseExploit = window.doJBwithPHOENIXLapseExploit = function() {
     runExploit();
+};
 
-}
+window.onload = function() {
+    /* Safe execution delay wrapper */
+    setTimeout(function() {
+        if (typeof window.log === "function") {
+            window.log("Auto-Start Trigger Blocked. Please interact with the controller UI.", "#888888");
+        }
+    }, 100);
+};
