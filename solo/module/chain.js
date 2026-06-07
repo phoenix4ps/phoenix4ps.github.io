5a609aad70db63c3b6a30f7344a115838ee9f509
phoenix4ps <basheermohammed3210@gmail.com>
2026-06-07 10:54:51 +0300

    Update chain.js

--- solo/module/chain.js
+++ solo/module/chain.js
@@ -15,10 +15,10 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
-import { Int, lohi_from_one } from './int64.mjs';
-import { get_view_vector } from './memtools.mjs';
-import { Addr } from './mem.mjs';
-import * as config from '../config.mjs';
+import { Int, lohi_from_one } from './int64.js';
+import { get_view_vector } from './memtools.js';
+import { Addr } from './mem.js';
+import * as config from '../js/config.js';
 
 // put the sycall names that you want to use here
 export const syscall_map = new Map(Object.entries({
@@ -196,13 +196,10 @@ export class ChainBase {
         this.full_stack_size = full_stack_size;
     }
 
-    // use this if you want to write a new ROP chain but don't want to allocate
-    // a new instance
     empty() {
         this.position = 0;
     }
 
-    // flag indicating whether .run() was ever called with this chain
     get is_dirty() {
         return this._is_dirty;
     }
@@ -237,7 +234,6 @@ export class ChainBase {
         return new Int(this._return_value[0], this._return_value[1]);
     }
 
-    // return value as a pointer
     get retval_ptr() {
         return new Addr(this._return_value[0], this._return_value[1]);
     }
@@ -285,297 +281,11 @@ export class ChainBase {
         this.position += 8;
     }
 
-    get_gadget(insn_str) {
-        const addr = this.gadgets.get(insn_str);
+    get_gadget(name) {
+        const addr = this.gadget_map.get(name);
         if (addr === undefined) {
-            throw Error(`gadget not found: ${insn_str}`);
+            throw Error(`gadget not found: ${name}`);
         }
-
         return addr;
     }
-
-    push_gadget(insn_str) {
-        this.push_value(this.get_gadget(insn_str));
-    }
-
-    push_call(func_addr, ...args) {
-        if (args.length > 6) {
-            throw TypeError(
-                'push_call() does not support functions that have more than 6'
-                + ' arguments');
-        }
-
-        for (let i = 0; i < args.length; i++) {
-            this.push_gadget(argument_pops[i]);
-            this.push_value(args[i]);
-        }
-
-        // The address of our buffer seems to be always aligned to 8 bytes.
-        // SysV calling convention requires the stack is aligned to 16 bytes on
-        // function entry, so push an additional 8 bytes to pad the stack. We
-        // pushed a "ret" gadget for a noop.
-        if ((this.position & (0x10 - 1)) !== 0) {
-            this.push_gadget('ret');
-        }
-
-        if (typeof func_addr === 'string') {
-            this.push_gadget(func_addr);
-        } else {
-            this.push_value(func_addr);
-        }
-    }
-
-    push_syscall(syscall_name, ...args) {
-        if (typeof syscall_name !== 'string') {
-            throw TypeError(`syscall_name not a string: ${syscall_name}`);
-        }
-
-        const sysno = syscall_map.get(syscall_name);
-        if (sysno === undefined) {
-            throw Error(`syscall_name not found: ${syscall_name}`);
-        }
-
-        const syscall_addr = this.syscall_array[sysno];
-        if (syscall_addr === undefined) {
-            throw Error(`syscall number not in syscall_array: ${sysno}`);
-        }
-
-        this.push_call(syscall_addr, ...args);
-    }
-
-    // Sets needed class properties
-    //
-    // Args:
-    //   gadgets:
-    //     A Map-like object mapping instruction strings (e.g. "pop rax; ret")
-    //     to their addresses in memory.
-    //   syscall_array:
-    //     An array whose indices correspond to syscall numbers. Maps syscall
-    //     numbers to their addresses in memory. Defaults to an empty Array.
-    static init_class(gadgets, syscall_array=[]) {
-        this.prototype.gadgets = gadgets;
-        this.prototype.syscall_array = syscall_array;
-    }
-
-    // START: implementation-dependent parts
-    //
-    // the user doesn't need to implement all of these. just the ones they need
-
-    // Firmware specific method to launch a ROP chain
-    //
-    // Proper implementations will check if .position is nonzero before
-    // running. Implementations can optionally check .is_dirty to enforce
-    // single-run gadget sequences
-    run() {
-        throw Error('not implemented');
-    }
-
-    // anything you need to do before the ROP chain jumps back to JavaScript
-    push_end() {
-        throw Error('not implemented');
-    }
-
-    push_get_errno() {
-        throw Error('not implemented');
-    }
-
-    push_clear_errno() {
-        throw Error('not implemented'); 
-    }
-
-    // get the rax register
-    push_get_retval() {
-        throw Error('not implemented');
-    }
-
-    // get the rax and rdx registers
-    push_get_retval_all() {
-        throw Error('not implemented');
-    }
-
-    // END: implementation-dependent parts
-
-    // note that later firmwares (starting around > 5.00?), the browser doesn't
-    // have a JIT compiler. we programmed in a way that tries to make the
-    // resulting bytecode be optimal
-    //
-    // we intentionally have an incomplete set (there's no function to get a
-    // full 128-bit result). we only implemented what we think are the common
-    // cases. the user will have to implement those other functions if they
-    // need it
-
-    do_call(...args) {
-        if (this.position) {
-            throw Error('chain not empty');
-        }
-        try {
-            this.push_call(...args);
-            this.push_get_retval();
-            this.push_get_errno();
-            this.push_end();
-            this.run();
-        } finally {
-            this.reset();
-        }
-    }
-
-    call_void(...args) {
-        this.do_call(...args);
-    }
-
-    call_int(...args) {
-        this.do_call(...args);
-        // x | 0 will always be a signed integer
-        return this._return_value[0] | 0;
-    }
-
-    call(...args) {
-        this.do_call(...args);
-        const retval = this._return_value;
-        return new Int(retval[0], retval[1]);
-    }
-
-    do_syscall(...args) {
-        if (this.position) {
-            throw Error('chain not empty');
-        }
-        try {
-            this.push_syscall(...args);
-            this.push_get_retval();
-            this.push_get_errno();
-            this.push_end();
-            this.run();
-        } finally {
-            this.reset();
-        }
-    }
-
-    syscall_void(...args) {
-        this.do_syscall(...args);
-    }
-
-    syscall_int(...args) {
-        this.do_syscall(...args);
-        // x | 0 will always be a signed integer
-        return this._return_value[0] | 0;
-    }
-
-    syscall(...args) {
-        this.do_syscall(...args);
-        const retval = this._return_value;
-        return new Int(retval[0], retval[1]);
-    }
-
-    syscall_ptr(...args) {
-        this.do_syscall(...args);
-        const retval = this._return_value;
-        return new Addr(retval[0], retval[1]);
-    }
-
-    // syscall variants that throw an error on errno
-
-    do_syscall_clear_errno(...args) {
-        if (this.position) {
-            throw Error('chain not empty');
-        }
-        try {
-            this.push_clear_errno();
-            this.push_syscall(...args);
-            this.push_get_retval();
-            this.push_get_errno();
-            this.push_end();
-            this.run();
-        } finally {
-            this.reset();
-        }
-    }
-
-    sysi(...args) {
-        const errno = this._errno;
-        this.do_syscall_clear_errno(...args);
-
-        const err = errno[0];
-        if (err !== 0) {
-            throw Error(`syscall(${args[0]}) errno: ${err}`);
-        }
-
-        // x | 0 will always be a signed integer
-        return this._return_value[0] | 0;
-    }
-
-    sys(...args) {
-        const errno = this._errno;
-        this.do_syscall_clear_errno(...args);
-
-        const err = errno[0];
-        if (err !== 0) {
-            throw Error(`syscall(${args[0]}) errno: ${err}`);
-        }
-
-        const retval = this._return_value;
-        return new Int(retval[0], retval[1]);
-    }
-
-    sysp(...args) {
-        const errno = this._errno;
-        this.do_syscall_clear_errno(...args);
-
-        const err = errno[0];
-        if (err !== 0) {
-            throw Error(`syscall(${args[0]}) errno: ${err}`);
-        }
-
-        const retval = this._return_value;
-        return new Addr(retval[0], retval[1]);
-    }
-
-}
-
-export function get_gadget(map, insn_str) {
-    const addr = map.get(insn_str);
-    if (addr === undefined) {
-        throw Error(`gadget not found: ${insn_str}`);
-    }
-
-    return addr;
-}
-
-function load_fw_specific(version) {
-    if (version & 0x10000) {
-        throw RangeError('ps5 not supported yet');
-    }
-
-    const value = version & 0xffff;
-    // we don't want to bother with very old firmwares that don't support
-    // ECMAScript 2015. 6.xx WebKit poisons the pointer fields of some types
-    // which can be annoying to deal with
-    if (value < 0x700) {
-        throw RangeError("PS4 firmwares < 7.00 isn't supported");
-    }
-
-    if (0x800 <= value && value <= 0x900) {
-        return import('../rop/900.mjs');
-    }
-
-    throw RangeError('firmware not supported');
-}
-
-export let gadgets = null;
-export let libwebkit_base = null;
-export let libkernel_base = null;
-export let libc_base = null;
-export let init_gadget_map = null;
-export let Chain = null;
-
-export async function init() {
-    const module = await load_fw_specific(config.target);
-    Chain = module.Chain;
-    module.init(Chain);
-    ({
-        gadgets,
-        libwebkit_base,
-        libkernel_base,
-        libc_base,
-        init_gadget_map,
-    } = module);
 }
